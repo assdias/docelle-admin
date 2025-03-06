7dbd0df1f48573d5675a9439c50a26908faf1c35
Adriano Dias <adrianosergiodias@gmail.com>
2025-03-05 22:29:55 -0300

    cadmin allowedHosts

--- medusa-config.ts
+++ medusa-config.ts
@@ -16,5 +16,11 @@ module.exports = defineConfig({
       jwtSecret: process.env.JWT_SECRET || "supersecret",
       cookieSecret: process.env.COOKIE_SECRET || "supersecret",      
     },
-  }
+  },
+  admin: {
+    vite: config => {
+      config.server.allowedHosts = ['*']
+      return config
+    }
+  }  
 })
